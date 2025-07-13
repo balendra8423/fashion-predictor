9284a934d5ac975639fa25daa65f7a41fbc16c3c
balendra8423 <balendrap12@gmail.com>
2025-07-13 22:01:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+import pickle as pkl
+
+model=pkl.load(open('model.pkl','rb'))
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+def preprocess_image(image):
+    img=Image.open(image)
+    img=img.resize((28,28))
+    img=img.convert('L')
+    img_array=np.array(img)/255.0
+    img_array=img_array.reshape(1,28,28,1)
+    return img_array
+
+st.title("🧥👗 Fashion Prediction 👠👚")
+
+
+uploaded_image=st.file_uploader("Upload an image",type=["jpg","jpeg","png"])
+
+if uploaded_image is not None:
+    image=Image.open(uploaded_image)
+    col1,col2=st.columns(2)
+    with col1:
+        resized_image=image.resize((200,200))
+        st.image(resized_image)
+    with col2:
+        if st.button("Classify"):
+            img_array=preprocess_image(uploaded_image)
+            result=model.predict(img_array)
+            predicted_class=np.argmax(result)
+            prediction=class_names[predicted_class]
+            st.success(f'Prediction: {prediction}')
+
+
+
+
